6ea7c7e715e9a2fe99727bb1b2f9c2cf14dfb26f
Mohsen Pouryazdan <mohs3n71@gmail.com>
2019-11-26 21:12:11 +0330

    Test 1

---
+++ precache-manifest.0002aa7b4e56642329ec5f03067309bf.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8c6c72dafe50b7654106285b1d4322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d54a1a91d92c51778c7b",
+    "url": "/static/css/2.a8f77c96.chunk.css"
+  },
+  {
+    "revision": "67ae89e94ae819486a18",
+    "url": "/static/css/main.633607cb.chunk.css"
+  },
+  {
+    "revision": "d54a1a91d92c51778c7b",
+    "url": "/static/js/2.46acf780.chunk.js"
+  },
+  {
+    "revision": "67ae89e94ae819486a18",
+    "url": "/static/js/main.d69cf2e6.chunk.js"
+  },
+  {
+    "revision": "ebe6113a340acdf79df1",
+    "url": "/static/js/runtime-main.c906fff4.js"
+  },
+  {
+    "revision": "c25ee6e0832083e39ceca0ac5a594223",
+    "url": "/static/media/android.c25ee6e0.svg"
+  },
+  {
+    "revision": "17b4426fb7fc08ad228522b5e5f5b293",
+    "url": "/static/media/books.17b4426f.svg"
+  },
+  {
+    "revision": "1479480114e2853cd791d0fea1a7aadf",
+    "url": "/static/media/drupal.14794801.svg"
+  },
+  {
+    "revision": "b5f88ba935856bebea3cafe688085701",
+    "url": "/static/media/facebook.b5f88ba9.svg"
+  },
+  {
+    "revision": "458d8774b1815d3fa7edd1bae48756c0",
+    "url": "/static/media/instagram.458d8774.svg"
+  },
+  {
+    "revision": "85015f7d7472260bd7caa3d05cf1b126",
+    "url": "/static/media/linkedin.85015f7d.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "f1703b6eede5052a8bbeb4fb6c0bfa76",
+    "url": "/static/media/telegram.f1703b6e.svg"
+  },
+  {
+    "revision": "79213201b6b71dd05f7659cd69154408",
+    "url": "/static/media/twitter.79213201.svg"
+  },
+  {
+    "revision": "827d1b491748f24511c605002ebf6f68",
+    "url": "/static/media/unity.827d1b49.svg"
+  },
+  {
+    "revision": "9537015d6a2c3a2347ef5bb98b5508f4",
+    "url": "/static/media/user.9537015d.svg"
+  },
+  {
+    "revision": "d3c59baa8590363d2e9121a6e8e6e5e1",
+    "url": "/static/media/workspace.d3c59baa.svg"
+  }
+]);
