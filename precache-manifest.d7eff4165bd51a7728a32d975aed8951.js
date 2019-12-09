930615dc5131b31f0ddf4ead2d48b47b1b05f3bc
Mohsen Pouryazdan <mohs3n71@gmail.com>
2019-12-09 12:45:23 +0330

    Revert "test new build" This reverts commit c674667d6a34b036c83a6be6769aac3ff1498c18.

---
+++ precache-manifest.d7eff4165bd51a7728a32d975aed8951.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e199e8dc7832a758c46f305b69e7c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9dffed6e0db9be28fc4",
+    "url": "/static/css/2.02f06fc8.chunk.css"
+  },
+  {
+    "revision": "9d3d8c4d42ba357ac9d0",
+    "url": "/static/css/main.fefadb43.chunk.css"
+  },
+  {
+    "revision": "b9dffed6e0db9be28fc4",
+    "url": "/static/js/2.019086fd.chunk.js"
+  },
+  {
+    "revision": "9d3d8c4d42ba357ac9d0",
+    "url": "/static/js/main.1c201475.chunk.js"
+  },
+  {
+    "revision": "ebe6113a340acdf79df1",
+    "url": "/static/js/runtime-main.c906fff4.js"
+  },
+  {
+    "revision": "c25ee6e0832083e39ceca0ac5a594223",
+    "url": "/static/media/android.c25ee6e0.svg"
+  },
+  {
+    "revision": "17b4426fb7fc08ad228522b5e5f5b293",
+    "url": "/static/media/books.17b4426f.svg"
+  },
+  {
+    "revision": "1479480114e2853cd791d0fea1a7aadf",
+    "url": "/static/media/drupal.14794801.svg"
+  },
+  {
+    "revision": "ab6aeb0aadb648621ed5928f467507a8",
+    "url": "/static/media/email.ab6aeb0a.svg"
+  },
+  {
+    "revision": "bc8c7aa458a227cb1662f5176ef1eeed",
+    "url": "/static/media/experience.bc8c7aa4.svg"
+  },
+  {
+    "revision": "b5f88ba935856bebea3cafe688085701",
+    "url": "/static/media/facebook.b5f88ba9.svg"
+  },
+  {
+    "revision": "458d8774b1815d3fa7edd1bae48756c0",
+    "url": "/static/media/instagram.458d8774.svg"
+  },
+  {
+    "revision": "85015f7d7472260bd7caa3d05cf1b126",
+    "url": "/static/media/linkedin.85015f7d.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "f1703b6eede5052a8bbeb4fb6c0bfa76",
+    "url": "/static/media/telegram.f1703b6e.svg"
+  },
+  {
+    "revision": "79213201b6b71dd05f7659cd69154408",
+    "url": "/static/media/twitter.79213201.svg"
+  },
+  {
+    "revision": "827d1b491748f24511c605002ebf6f68",
+    "url": "/static/media/unity.827d1b49.svg"
+  },
+  {
+    "revision": "9537015d6a2c3a2347ef5bb98b5508f4",
+    "url": "/static/media/user.9537015d.svg"
+  },
+  {
+    "revision": "d3c59baa8590363d2e9121a6e8e6e5e1",
+    "url": "/static/media/workspace.d3c59baa.svg"
+  }
+]);
